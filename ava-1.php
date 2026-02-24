194c1619df0886944f36e5d2188cf58265ca7097
Sophia Roque Martins <martinssophia983@gmail.com>
2026-02-24 17:32:24 -0300

    Update ava-1.php

--- ava-1.php
+++ ava-1.php
@@ -8,30 +8,40 @@
 <body>
 <?php 
 class Carro {
+    //ATRIBUTOS 
 public $marca;
 public $modelo;
 public $ano;
 public $cor;
 public $velocidade= 0;
-
+ // FUNÇÃO - ACELERAR 
 public function acelerar (){
 $this->velocidade +=10;
  echo "O carro acelerou. Velocidade: " . $this->velocidade. "Km/h <br>";
 }
+//FUNÇÃO - FREAR 
 public function frear(){
 if ($this ->velocidade >=10){
     $this -> velocidade -=10;
 }
 echo "O carro freou. Velocidade: " . $this ->velocidade. "Km/h <br>";
 }
+//FUNÇÃO -  MARCA 
+public function marca (){
+$this->marca ="FERRARI ";
+ echo "A marca do carro é :  " . $this->marca;
 }
-//objeto
+
+}
+//OBJETO 
 $meuCarro=New Carro();
 
-//metodos 
+//METODOS 
 $meuCarro -> acelerar();
 $meuCarro -> acelerar();
 $meuCarro -> frear();
+$meuCarro -> marca();
+
 
 
 ?>
